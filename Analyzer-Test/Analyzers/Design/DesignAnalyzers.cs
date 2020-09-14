9f4c83a75b4563208a84b57e5e32b9b1cb8bffe3
Ko1ors <kolorsnik181200@gmail.com>
2020-09-14 22:46:23 +0300

    Design anylyzers update 2

--- Analyzer-Test/Analyzers/Design/DesignAnalyzers.cs
+++ Analyzer-Test/Analyzers/Design/DesignAnalyzers.cs
@@ -11,14 +11,38 @@ namespace Analyzer_Test.Analyzers.Design
 {
     class DesignAnalyzers
     {
+        private static Dictionary<SyntaxNode, List<String>> report = new Dictionary<SyntaxNode, List<string>>();
         public static void Analyze(SyntaxNode node)
         {
             if (node.IsKind(SyntaxKind.CatchClause))
             {
                 if (CatchEmptyAnalyzer.Analyze(node))
-                    Console.WriteLine(node.Parent.Parent);
+                {
+                    ReportAdd(GetNodeClass(node), "Contains empty catch.");
+                }
             }
         }
 
+        private static void ReportAdd(SyntaxNode node, String result)
+        {
+            if (report.TryGetValue(node, out var results))
+                results.Add(result);
+            else
+                report.Add(node,new List<string> { result });
+            Console.WriteLine((node as TypeDeclarationSyntax).Identifier.ValueText);
+        }
+
+        public static SyntaxNode GetNodeClass(SyntaxNode node)
+        {
+            var tree = node.SyntaxTree;
+            if (tree is null)
+                throw new Exception();
+            return tree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>().Last();
+        }
+
+        public static Dictionary<SyntaxNode, List<String>> GetReport()
+        {
+            return report;
+        }
     }
 }
