71d49252aa144a22a7ff391fb09a722b85ebf17f
Ko1ors <kolorsnik181200@gmail.com>
2020-09-14 20:34:44 +0300

    Stable

--- Analyzer-Test/MainWindow.xaml.cs
+++ Analyzer-Test/MainWindow.xaml.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeMetrics;
+using System;
 using System.Collections.Immutable;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Xml;
-using Microsoft.Build.Locator;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeMetrics;
-using Microsoft.CodeAnalysis.MSBuild;
 
 
 namespace Analyzer_Test
@@ -32,10 +26,11 @@ namespace Analyzer_Test
         public void Create()
         {
             MSBuildLocator.RegisterDefaults();
-            var ws = Microsoft.CodeAnalysis.MSBuild.MSBuildWorkspace.Create(new Dictionary<string, string> { { "CheckForSystemRuntimeDependency", "true" } });
-           // ws.LoadMetadataForReferencedProjects = true;
-            var slnTask = ws.OpenSolutionAsync(@"C:\Users\Ko1ors\source\repos\Ko1ors\PPZTicketsModel\PPZModel.sln");
+            var ws = Microsoft.CodeAnalysis.MSBuild.MSBuildWorkspace.Create();
+            // ws.LoadMetadataForReferencedProjects = true;
+            Task<Solution> slnTask = ws.OpenSolutionAsync(@"C:\Users\Ko1ors\source\repos\Ko1ors\InventoryPrice\InventoryPrice.sln");
             slnTask.Wait();
+
             var sln = slnTask.Result;
             var builder = ImmutableArray.CreateBuilder<(string, CodeAnalysisMetricData)>();
             foreach (var project in sln.Projects.ToList())
