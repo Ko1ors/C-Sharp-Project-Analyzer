3429675543b09d41e403a6ab841efcf6978a6c79
Ko1ors <kolorsnik181200@gmail.com>
2020-11-02 15:49:00 +0200

    AddBracesToSwitchSectionsAnalyzer and UseConfigureAwaitFalseAnalyzer analyzers added

---
+++ Analyzer-Test/Analyzers/Single Analyzers/Refactoring/AddBracesToSwitchSectionsAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyzer_Test.Analyzers.Single_Analyzers.Perfomance
+{
+    public class AddBracesToSwitchSectionsAnalyzer : AbstractAnalyzer
+    {
+        public override bool Analyze(SyntaxNode node, Data.SolutionInfo si)
+        {
+            if (node.IsGenerated()) return false;
+            var @switch = (SwitchStatementSyntax)node;
+            if (!@switch.Sections.All(HasBraces))
+                return true;
+            return false;
+        }
+
+        private static bool HasBraces(SwitchSectionSyntax section)
+        {
+            switch (section.Statements.Count)
+            {
+                case 1:
+                    if (section.Statements.First() is BlockSyntax)
+                        return true;
+                    break;
+                case 2:
+                    if (section.Statements.First() is BlockSyntax && section.Statements.Last() is BreakStatementSyntax)
+                        return true;
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+
+        public override bool CheckConditionals(SyntaxNode node)
+        {
+            if (node.IsKind(SyntaxKind.SwitchStatement))
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Analyzer-Test/Analyzers/Single Analyzers/Reliability/UseConfigureAwaitFalseAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyzer_Test.Analyzers.Single_Analyzers.Reliability
+{
+    public class UseConfigureAwaitFalseAnalyzer : AbstractAnalyzer
+    {
+        public override bool Analyze(SyntaxNode node, Data.SolutionInfo si)
+        {
+            if (node.IsGenerated()) return false;
+            var awaitExpression = (AwaitExpressionSyntax)node;
+            var awaitedExpression = awaitExpression.Expression;
+            if (!IsTask(awaitedExpression, node, si))
+                return false;
+            return true;
+        }
+        private static bool IsTask(ExpressionSyntax expression, SyntaxNode node, Data.SolutionInfo si)
+        {
+            var semanticModel = si.GetCurrentCompilation().GetSemanticModel(node.SyntaxTree);
+            var type = semanticModel.GetTypeInfo(expression).Type as INamedTypeSymbol;
+            if (type == null)
+                return false;
+            INamedTypeSymbol taskType;
+
+            if (type.IsGenericType)
+            {
+                type = type.ConstructedFrom;
+                taskType = semanticModel.Compilation.GetTypeByMetadataName("System.Threading.Tasks.Task`1");
+            }
+            else
+            {
+                taskType = semanticModel.Compilation.GetTypeByMetadataName("System.Threading.Tasks.Task");
+            }
+            return type.Equals(taskType);
+        }
+
+        public override bool CheckConditionals(SyntaxNode node)
+        {
+            if (node.IsKind(SyntaxKind.AwaitExpression))
+                return true;
+            return false;
+        }
+    }
+}
