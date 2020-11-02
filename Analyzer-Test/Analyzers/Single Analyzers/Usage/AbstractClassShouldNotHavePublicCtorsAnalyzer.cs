13247a58db62b11fbd2f089768840585b394fc13
Ko1ors <kolorsnik181200@gmail.com>
2020-11-02 16:04:20 +0200

    AbstractClassShouldNotHavePublicCtorsAnalyzer analyzer added

---
+++ Analyzer-Test/Analyzers/Single Analyzers/Usage/AbstractClassShouldNotHavePublicCtorsAnalyzer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyzer_Test.Analyzers.Single_Analyzers.Usage
+{
+    public class AbstractClassShouldNotHavePublicCtorsAnalyzer : AbstractAnalyzer
+    {
+        public override bool Analyze(SyntaxNode node, Data.SolutionInfo si)
+        {
+            if (node.IsGenerated()) 
+                return false;
+            var ctor = (ConstructorDeclarationSyntax)node;
+            if (!ctor.Modifiers.Any(m => m.IsKind(SyntaxKind.PublicKeyword))) 
+                return false;
+            var @class = ctor.Ancestors().FirstOrDefault() as ClassDeclarationSyntax;
+            if (@class == null) 
+                return false;
+            if (!@class.Modifiers.Any(m => m.IsKind(SyntaxKind.AbstractKeyword))) 
+                return false;
+            return true;
+        }
+
+        public override bool CheckConditionals(SyntaxNode node)
+        {
+            if (node.IsKind(SyntaxKind.ConstructorDeclaration))
+                return true;
+            return false;
+        }
+    }
+}
