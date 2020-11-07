55189cfbeaea93dbd9ef97d27d6e69e31935b4fc
Ko1ors <kolorsnik181200@gmail.com>
2020-11-07 16:00:42 +0200

    MaintainabilityIndexMiniUC update

--- Analyzer-Test/UI/UserControls/MaintainabilityIndexMiniUC.xaml.cs
+++ Analyzer-Test/UI/UserControls/MaintainabilityIndexMiniUC.xaml.cs
@@ -20,9 +20,21 @@ namespace Analyzer_Test.UI.UserControls
     /// </summary>
     public partial class MaintainabilityIndexMiniUC : UserControl
     {
+        public static readonly DependencyProperty ClassNameProperty = DependencyProperty.Register("ClassName", typeof(String), typeof(MaintainabilityIndexMiniUC));
+        public String ClassName
+        {
+            get { return this.GetValue(ClassNameProperty).ToString(); }
+            set { this.SetValue(ClassNameProperty, value); }
+        }
         public MaintainabilityIndexMiniUC()
         {
             InitializeComponent();
+        }
+
+        public MaintainabilityIndexMiniUC(String className)
+        {
+            InitializeComponent();
+            ClassName = className;
         }
     }
 }
